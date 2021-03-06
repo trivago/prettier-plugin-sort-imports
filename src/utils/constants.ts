334837503bcc694ba9e8aeb0c183558585f6d064
Behrang Yarahmadi <behrang.yarahmdi@trivago.com>
2021-03-06 20:37:32 +0100

    add parser names as constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const flow = 'flow';
+export const babelFlow = 'babel-flow';
+export const typescript = 'typescript';
+export const decoratorsLegacy = 'decorators-legacy';
+export const classProperties = 'classProperties';
+export const jsx = 'jsx';
--- src/utils/get-parser-plugins.ts
+++ src/utils/get-parser-plugins.ts
@@ -1,19 +1,22 @@
 import { BuiltInParserName, CustomParser } from 'prettier';
+import {
+    flow,
+    babelFlow,
+    typescript,
+    decoratorsLegacy,
+    classProperties,
+    jsx,
+} from './constants';
 
 export const getParserPlugins = (
     prettierParser: BuiltInParserName | CustomParser,
 ) => {
-    const isFlow = prettierParser === 'flow' || prettierParser === 'babel-flow';
-    const isTypescript = prettierParser === 'typescript';
+    const isFlow = prettierParser === flow || prettierParser === babelFlow;
+    const isTypescript = prettierParser === typescript;
 
-    const tsPlugins = [
-        'typescript',
-        'jsx',
-        'decorators-legacy',
-        'classProperties',
-    ];
+    const tsPlugins = [typescript, jsx, decoratorsLegacy, classProperties];
 
-    return [isFlow && 'flow', ...(isTypescript ? tsPlugins : [])].filter(
+    return [isFlow && flow, ...(isTypescript ? tsPlugins : [])].filter(
         Boolean,
     );
 };
