84904799cc55c5aea0dfc689e6f165bb30cd5394
Raine Revere <raine@cybersemics.org>
2021-07-14 10:47:04 -0600

    Fix faulty comment replace in removeNodesFromOriginalCode (#55)

--- src/utils/remove-nodes-from-original-code.ts
+++ src/utils/remove-nodes-from-original-code.ts
@@ -6,6 +6,11 @@ import {
     InterpreterDirective,
 } from '@babel/types';
 
+/** Escapes a string literal to be passed to new RegExp. See: https://stackoverflow.com/a/6969486/480608.
+ * @param s the string to escape
+ */
+const escapeRegExp = (s: string) => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
 /**
  * Removes imports from original file
  * @param code the whole file as text
@@ -26,7 +31,15 @@ export const removeNodesFromOriginalCode = (
         const start = Number(node.start);
         const end = Number(node.end);
         if (Number.isSafeInteger(start) && Number.isSafeInteger(end)) {
-            text = text.replace(code.substring(start, end), '');
+            text = text.replace(
+                // only replace imports at the beginning of the line (ignoring whitespace)
+                // otherwise matching commented imports will be replaced
+                new RegExp(
+                    '^\\s*' + escapeRegExp(code.substring(start, end)),
+                    'm',
+                ),
+                '',
+            );
         }
     }
     return text;
