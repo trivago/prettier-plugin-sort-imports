fbab9be75be9b80638c9ab306f95e9678114bbd4
Saarya <saarya@gmail.com>
2021-10-13 19:24:27 +0300

    Clean up extra spaces

--- src/utils/__tests__/get-experimental-parser-plugins.spec.ts
+++ src/utils/__tests__/get-experimental-parser-plugins.spec.ts
@@ -1,10 +1,8 @@
 import { getExperimentalParserPlugins } from '../get-experimental-parser-plugins';
 
-
 test('it should return empty list', () => {
     expect(getExperimentalParserPlugins([])).toEqual([]);
 });
-
 
 test('it should return flow and decorators', () => {
     expect(getExperimentalParserPlugins(['flow', 'decorators'])).toEqual(['flow', 'decorators']);
@@ -14,7 +12,6 @@ test('it should return decorators with parsed options', () => {
     expect(getExperimentalParserPlugins(['["decorators", { "decoratorsBeforeExport": true }]']))
         .toEqual([['decorators', { decoratorsBeforeExport: true }]]);
 });
-
 
 test('it should return decorators with parsed options', () => {
     expect(getExperimentalParserPlugins(['flow', '["decorators", { "decoratorsBeforeExport": true }]']))
