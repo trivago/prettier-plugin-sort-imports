29da002f13c9b07925f53788dceadfee33eef000
Saarya <saarya@gmail.com>
2021-10-13 19:44:08 +0300

    Fix comment

--- src/types.ts
+++ src/types.ts
@@ -3,5 +3,5 @@ import { RequiredOptions } from 'prettier';
 export interface PrettierOptions extends RequiredOptions {
     importOrder: string[];
     importOrderSeparation: boolean;
-    experimentalBabelParserPluginsList: string[]; // should be of type ParserPlugin from '@babel/parser' but babel does not support nested arrays in options
+    experimentalBabelParserPluginsList: string[]; // should be of type ParserPlugin from '@babel/parser' but prettier does not support nested arrays in options
 }
