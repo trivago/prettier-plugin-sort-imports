cfa685c273eb9e8233247027d4fb03c62a2943c0
byara <behrang.yarahmadi@trivago.com>
2021-10-25 11:19:14 +0200

    Remove experimentalBabelParserPluginsList as API

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,6 @@
 import { RequiredOptions } from 'prettier';
 
 export interface PrettierOptions extends RequiredOptions {
-    // should be of type ParserPlugin from '@babel/parser' but prettier does not support nested arrays in options
-    experimentalBabelParserPluginsList: string[];
     importOrder: string[];
     importOrderCaseInsensitive: boolean;
     // should be of type ParserPlugin from '@babel/parser' but prettier does not support nested arrays in options
