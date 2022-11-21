daadfadbce4ec9c763f248686b307f5fd4da38bf
Robert Kieffer <robert@broofa.com>
2022-11-21 10:57:40 -0800

    add separated test, fixup both tests

--- tests/ImportsSeparated/imports-with-directives.ts
+++ tests/ImportsSeparated/imports-with-directives.ts
@@ -1,8 +1,12 @@
 'use strict';
 'use client';
+
+// comment after directives
 import otherthing from "@core/otherthing";
 import abc from "@core/abc";
+
 // Comment
+
 function add(a:number,b:number) {
-    return a + b;
+  return a + b;
 }
