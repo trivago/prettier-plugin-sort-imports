b209a630539208a097c638d3a50b65eb28d6aed3
dante <c.dante.federici@gmail.com>
2022-12-22 15:44:06 -0500

    Only preserve directives already at the start of the program

--- src/utils/extract-ast-nodes.ts
+++ src/utils/extract-ast-nodes.ts
@@ -11,11 +11,20 @@ export function extractASTNodes(ast: ParseResult<File>) {
     const importNodes: ImportDeclaration[] = [];
     const directives: Directive[] = [];
     traverse(ast, {
-        Directive({ node }) {
-            directives.push(node);
+        Directive(path: NodePath<Directive>) {
+            // Only capture directives if they are at the top scope of the source
+            // and their previous siblings are all directives
+            if (
+                path.parent.type === 'Program' &&
+                path.getAllPrevSiblings().every((s) => {
+                    return s.type === 'Directive';
+                })
+            ) {
+                directives.push(path.node);
 
-            // Trailing comments probably shouldn't be attached to the directive
-            node.trailingComments = null;
+                // Trailing comments probably shouldn't be attached to the directive
+                path.node.trailingComments = null;
+            }
         },
 
         ImportDeclaration(path: NodePath<ImportDeclaration>) {
