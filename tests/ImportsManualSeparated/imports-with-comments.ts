18aa9dc56b7e9b8d4069adbe08ae3d0f149642bf
Emily M Klassen <forivall@gmail.com>
2024-11-08 13:58:23 -0800

    Add tests for the separator special word

---
+++ tests/ImportsManualSeparated/imports-with-comments.ts
@@ -0,0 +1,10 @@
+// I am top level comment in this file.
+// I am second line of top level comment in this file.
+import './commands';
+
+// Comment
+// Comment
+
+function add(a:number,b:number) {
+    return a + b;
+}
