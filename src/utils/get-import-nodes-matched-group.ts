63fe3f5b26a2fe2eea206cb6d6b4ac29472ff894
Behrang Yarahmadi <behrang.yarahmadi@trivago.com>
2024-12-09 11:33:41 +0100

    v5.2.0 (#334)
    * Fix conditional import of prettier-plugin-svelte (#332)
    
    * Fix type import ordering (#331)
    
    * fix type import ordering
    
    * Add a snapshot test for the bugfix
    
    * clean up
    
    ---------
    
    Co-authored-by: Russell Dunphy <russell@russelldunphy.com>

--- src/utils/get-import-nodes-matched-group.ts
+++ src/utils/get-import-nodes-matched-group.ts
@@ -22,21 +22,35 @@ export const getImportNodesMatchedGroup = (
             : new RegExp(group),
     }));
 
-    for (const { group, regExp } of groupWithRegExp) {
+    // finding the group for non-type imports is easy: it's the first group that matches.
+    // however, for type imports, we need to make sure that we don't match a non-type group
+    // that's earlier in the list than a type-specific group that would otherwise match.
+    // so we need to get all matching groups, look for the first matching _type-specific_ group,
+    // and if it exists, return it. otherwise, return the first matching group if there is one.
+    const matchingGroups = groupWithRegExp.filter(({ group, regExp }) => {
         if (
-            (group.startsWith(TYPES_SPECIAL_WORD) &&
-                node.importKind !== 'type') ||
-            (!group.startsWith(TYPES_SPECIAL_WORD) &&
-                node.importKind === 'type')
-        )
-            continue;
+            group.startsWith(TYPES_SPECIAL_WORD) &&
+            node.importKind !== 'type'
+        ) {
+            return false;
+        } else {
+            return node.source.value.match(regExp) !== null;
+        }
+    });
 
-        const matched = node.source.value.match(regExp) !== null;
-        if (matched) return group;
+    if (matchingGroups.length === 0) {
+        return node.importKind === 'type' &&
+            importOrder.find(
+                (group) => group === THIRD_PARTY_TYPES_SPECIAL_WORD,
+            )
+            ? THIRD_PARTY_TYPES_SPECIAL_WORD
+            : THIRD_PARTY_MODULES_SPECIAL_WORD;
+    } else if (node.importKind !== 'type') {
+        return matchingGroups[0].group;
+    } else {
+        for (const { group } of matchingGroups) {
+            if (group.startsWith(TYPES_SPECIAL_WORD)) return group;
+        }
+        return matchingGroups[0].group;
     }
-
-    return node.importKind === 'type' &&
-        importOrder.find((group) => group === THIRD_PARTY_TYPES_SPECIAL_WORD)
-        ? THIRD_PARTY_TYPES_SPECIAL_WORD
-        : THIRD_PARTY_MODULES_SPECIAL_WORD;
 };
