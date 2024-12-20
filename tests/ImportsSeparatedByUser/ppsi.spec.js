b9c05b60cddc388e2798392d44245fb540780cf3
Chason Choate <chason.choate@trillianthealth.com>
2024-12-19 22:32:06 -0600

    Added current-state snapshots
    Capturing the current state of transformations so we can see how the
    upcoming changes affect them.

---
+++ tests/ImportsSeparatedByUser/ppsi.spec.js
@@ -0,0 +1,5 @@
+run_spec(__dirname, ["typescript"], {
+    importOrder: ['^@core/(.*)$', '^@server/(.*)', '^@ui/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderParserPlugins: ['typescript']
+});
