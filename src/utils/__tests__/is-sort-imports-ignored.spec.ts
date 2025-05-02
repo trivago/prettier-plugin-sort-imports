d07ad76d68db5999a5225b34dc88520faefb7e0c
Cullen Walsh <ckwalsh@cullenwalsh.com>
2025-05-02 01:46:09 +0000

    [Feature] Expand detection for `sort-imports-ignore`
    Summary:
    Prior to this commit, isSortImportsIgnored() checked comments
    associated with the extracted ImportDirectives, and only if the
    comment started on line 1. This could result in a failure to suppress
    sorting if the comment was not next to an ImportDirective (never
    considered), the first import directive were embedded later in the
    file (line mismatch), or directives/shebangs were used (Line 1 is
    unavailable).
    
    With this change, the line restriction is removed, and all comments
    from the beginning of the file and the first statement are checked.
    This ensures better coverage, especially with the
    importOrderIgnoreHeaderComments feature stacked on this commit.
    
    Test Plan:
    `yarn install && yarn run test --all`

--- src/utils/__tests__/is-sort-imports-ignored.spec.ts
+++ src/utils/__tests__/is-sort-imports-ignored.spec.ts
@@ -1,3 +1,4 @@
+import { getAllCommentsFromNodes } from '../get-all-comments-from-nodes';
 import { getImportNodes } from '../get-import-nodes';
 import { isSortImportsIgnored } from '../is-sort-imports-ignored';
 
@@ -13,11 +14,15 @@ import z from 'z';
 test('it should return true if specific leading comment detected', () => {
     const importNodes = getImportNodes(codeIgnored);
 
-    expect(isSortImportsIgnored(importNodes)).toBeTruthy();
+    expect(
+        isSortImportsIgnored(getAllCommentsFromNodes(importNodes)),
+    ).toBeTruthy();
 });
 
 test('it should return false if no specific leading comment detected', () => {
     const importNodes = getImportNodes(codeNotIgnored);
 
-    expect(isSortImportsIgnored(importNodes)).toBeFalsy();
+    expect(
+        isSortImportsIgnored(getAllCommentsFromNodes(importNodes)),
+    ).toBeFalsy();
 });
