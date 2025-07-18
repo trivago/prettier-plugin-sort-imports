4955f1c948ee85bba1842d96c44b607b129dd8f4
Cullen Walsh <ckwalsh@cullenwalsh.com>
2025-07-18 19:43:50 +0000

    [Feature] importOrderIgnoreHeaderComments option
    Summary:
    This commit adds the importOrderIgnoreHeaderComments options and all associated implementation.
    
    The importOrderIgnoreHeaderComments option allows the plugin to
    recognize that the first comments in code may be header files, and
    preserve their contents and whitespace. This can help avoid
    situations where this plugin and other tools conflict with expected
    whitespace, resulting in formatter cycles.
    
    Test Plan:
    `yarn install && yarn run test --all`
    
    Note: This diff adds new snapshot tests to cover the new functionality.

---
+++ tests/HeaderCommentsBlock/single-block-header.ts
@@ -0,0 +1,19 @@
+/* Copyright header comment */
+
+// First import attached CommentLine
+import thirdParty0 from "third-party0";
+import something3 from "@core/something3";
+import thirdDisco0 from "third-disco0";
+import otherthing3 from "@core/otherthing3";
+// CommentLine attached to import
+import { a } from "b" with { type: "json" };
+
+import anotherSameLevelRelativePath3 from "./anotherSameLevelRelativePath3";
+import something0 from "@core/something0";
+
+import { b } from "r" with { type: "json" };
+
+
+function add(a,b) {
+  return a + b;
+}
