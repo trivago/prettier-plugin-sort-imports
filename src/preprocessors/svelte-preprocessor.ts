71ff357607abde38ab71cdb2d7aeecb5944ab90b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-28 14:55:05 -0400

    Revert extraneous svelte change

--- src/preprocessors/svelte-preprocessor.ts
+++ src/preprocessors/svelte-preprocessor.ts
@@ -1,25 +1,25 @@
-import { PrettierOptions } from '../types';
-import { preprocessor } from './preprocessor.js';
-
 let prettierPluginSvelte: typeof import('prettier-plugin-svelte') | undefined;
 let svelteCompiler: typeof import('svelte/compiler') | undefined;
 
 try {
-    prettierPluginSvelte = await import('prettier-plugin-svelte');
+    prettierPluginSvelte = await import('prettier-plugin-svelte')
     svelteCompiler = await import('svelte/compiler');
 } catch {
     // Do not error because the dependency is optional.
 }
+
+import { PrettierOptions } from '../types';
+import { preprocessor } from './preprocessor.js';
 
 const booleanGuard = <T>(value: T | undefined): value is T => Boolean(value);
 
 const sortImports = (code: string, options: PrettierOptions) => {
     if (!svelteCompiler) {
         throw new Error(
-            "Missing peer dependency 'svelte/compiler'. Please install it to use the svelte parser.",
+        "Missing peer dependency 'svelte/compiler'. Please install it to use the svelte parser.",
         );
     }
-
+    
     const { parse } = svelteCompiler;
     const { instance, module } = parse(code);
     const sources = [instance, module].filter(booleanGuard);
@@ -38,7 +38,7 @@ export function sveltePreprocessor(code: string, options: PrettierOptions) {
 
     if (!prettierPluginSvelte) {
         throw new Error(
-            "Missing peer dependency 'prettier-plugin-svelte'. Please install it to use the svelte parser.",
+        "Missing peer dependency 'prettier-plugin-svelte'. Please install it to use the svelte parser.",
         );
     }
     // @ts-expect-error TODO: Fix this type error
