9a63840da7fe7a1b40a281d06c38045543554f4b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-28 14:36:26 -0400

    hm

--- src/preprocessors/ember-preprocessor.ts
+++ src/preprocessors/ember-preprocessor.ts
@@ -1,35 +1,30 @@
+import { parseImportsExports } from 'parse-imports-exports';
+
 import { PrettierOptions } from '../types';
 import { preprocessor } from './preprocessor.js';
 
-// @ts-expect-error
-let prettierPluginEmberTemplateTag;
-
-try {
-    emberPreprocessor = await import(
-        // @ts-expect-error
-        'prettier-plugin-ember-template-tag/dist/'
-    );
-} catch {
-    // Do not error because the dependency is optional.
-}
-
-const booleanGuard = <T>(value: T | undefined): value is T => Boolean(value);
-
 const sortImports = (code: string, options: PrettierOptions) => {
-    // @ts-expect-error
-    if (!prettierPluginEmberTemplateTag) {
-        throw new Error(
-            "Missing peer dependency 'prettier-plugin-ember-template-tag'. Please install it to use the ember-template-tag parser.",
-        );
-    }
+  const importsExports = parseImportsExports(code, { ignoreDynamicImports: true, ignoreRegexpLiterals: true, ignoreRequires: true, ignoreCommonJsExports: true });
 
-    const { parsers } = (prettierPluginEmberTemplateTag.default ||
-        prettierPluginEmberTemplateTag) as {
-        parsers: { 'ember-template-tag': any };
-    };
-    const ast = parsers['ember-template-tag'].parse(code, options);
-    
-    
+  // console.log(importsExports);
+
+  let justImports = '';
+
+  for (let [path, info] of Object.entries({...importsExports.namedImports})) {
+    console.log(path, info);
+  }
+
+  if (importsExports.defaultExport) {
+    let  pos = importsExports.defaultExport;
+    let text = code.slice(pos.start, pos.end);
+    justImports += text;
+  }
+
+  for (let [path, info] of Object.entries(importsExports.namedImports)) {
+    console.log(path, info);
+  }
+
+  return preprocessor(justImports, options);
 };
 
 export function emberPreprocessor(code: string, options: PrettierOptions) {
