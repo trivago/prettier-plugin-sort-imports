7db414f43ed4aa8825f1287da1383fce447201f0
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-29 12:36:40 -0400

    Reduce config

--- tests/Ember/ppsi.spec.mjs
+++ tests/Ember/ppsi.spec.mjs
@@ -3,6 +3,5 @@ import plugin from '../../src';
 run_spec(__dirname, ['ember-template-tag'], {
     importOrder: ['^@core/(.*)$', '^@server/(.*)', '^@ui/(.*)$', '^[./]'],
     importOrderSeparation: true,
-    importOrderParserPlugins : ['typescript'],
     plugins: ['prettier-plugin-ember-template-tag', plugin]
 });
