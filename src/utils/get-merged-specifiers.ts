47f69555627b371ecb3a6e88dd2d4adb26715f1d
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2026-03-27 00:07:46 +0000

    add comments and tweak naming

--- src/utils/get-merged-specifiers.ts
+++ src/utils/get-merged-specifiers.ts
@@ -1,25 +1,30 @@
 import type { ImportDeclaration } from '@babel/types';
 
+/**
+ * This function merges the specifiers of import nodes that have the same source
+ * into a single import declaration.
+ * @param node All imports nodes that should be merged.
+ */
 export function getMergedSpecifiers(nodes: ImportDeclaration[]) {
-    // Combine import specifiers
     const merged = nodes.reduce<typeof nodes>((acc, node) => {
         if (node.specifiers.length === 0) {
             acc.push(node);
             return acc;
         }
 
-        const exists = acc.find((n) => {
-            return (
+        const nodeToMerge = acc.find(
+            (n) =>
                 n.source.value === node.source.value &&
                 // ignore import if it's a namespace specifier
-                n.specifiers[0]?.type !== 'ImportNamespaceSpecifier'
-            );
-        });
-        if (exists) {
-            exists.specifiers.push(...node.specifiers);
+                n.specifiers[0]?.type !== 'ImportNamespaceSpecifier',
+        );
+
+        if (nodeToMerge) {
+            nodeToMerge.specifiers.push(...node.specifiers);
         } else {
             acc.push(node);
         }
+
         return acc;
     }, []);
 
